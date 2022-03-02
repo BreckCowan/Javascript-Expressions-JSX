0fe11ce26dec1df0de3341fff26a7b88c58f958f
Breck Cowan <breckcowan@gmail.com>
2022-03-02 11:44:13 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Breck";
+const lName = "Cowan";
+// const number = 11;
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    {/* template literals */}
+    <h1>Hello {`${fName} ${lName}`}!</h1>
+    {/* String concatination */}
+    <h1>Hello {fName + " " + lName}!</h1>
+
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
+  </div>,
+
+  document.getElementById("root")
+);
